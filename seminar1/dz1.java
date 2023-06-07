eca4f950b923da03dcc88542f52d6ebf19419e45
Muzhait <muzhait@mail.ru>
2023-06-07 13:16:20 +0300

    dz 1 seminar

---
+++ seminar1/dz1.java
@@ -0,0 +1,18 @@
+package seminar1;
+// Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+import java.util.Scanner;
+
+public class dz1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        int i = iScanner.nextInt();
+        System.out.printf("N-ое треугольного числа: %d\n", giveMeNumber(i));
+        iScanner.close();
+    }
+
+    public static int giveMeNumber(int a) {
+        return (a * (a + 1)) / 2;
+    }
+}
